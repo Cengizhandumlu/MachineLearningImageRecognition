0e96936ab0a3adb6156b1d8fea6a40300da7081d
Cengizhan DUMLU <cengizhandumlu.35@gmail.com>
2021-04-06 18:47:20 +0300

    Initial Commit

---
+++ MachineLearningImageRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImageRecognition
+//
+//  Created by Cengizhan DUMLU on 6.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
